d0fe5f05acaf5ffc73b0ef22602a076b90f019f8
Karthi~Natarajan <136811565+Karthi-Natarajan@users.noreply.github.com>
2026-02-17 00:26:40 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://employee-attendance-system-vdkh.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
@@ -10,24 +10,17 @@ const api = axios.create({
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
-    console.log('Axios request to:', config.url, 'Token present:', !!token);
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
-      console.log('Authorization header added');
-    } else {
-      console.log('WARNING: No token found in localStorage');
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    console.log('Response error:', error.response?.status, error.response?.data);
     if (error.response?.status === 401) {
       localStorage.removeItem("token");
       window.location.href = "/login";
